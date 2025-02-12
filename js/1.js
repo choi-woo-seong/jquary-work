df6de66288eaaf7d7e86b47688942f64c03c4c9c
choi-woo-seong <goqus903@naver.com>
2025-02-12 09:42:48 +0900

    first

---
+++ js/1.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("h1").click(function(){
+    $(this).hide();
+  })
+  
+  $(".abc").click(function(){
+    $(".abc").hide();
+  })
+  
+  $("#name").click(function(){
+    $("#name").hide();
+  })
+})
---
+++ js/6.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  // $("#btn_menu").on("click",function(){
+  //   $(".div_menu").slideDown('slow')
+  // })
+
+  // $("#btn_menu").on("click",function(){
+  //   $(".div_menu").slideUp('slow')
+  // })
+
+  $("#btn_menu").on("click",function(){
+    $(".div_menu").slideToggle('slow')
+  })
+})
