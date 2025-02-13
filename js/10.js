f2be15c0c4eb4ae0bbad83a76636d85e97f0ccd2
choi-woo-seong <goqus903@naver.com>
2025-02-13 10:42:54 +0900

    loop

--- js/10.js
+++ js/10.js
@@ -7,4 +7,41 @@ $(document).ready(function(){
     { name : "이서", age : 18},
     { name : "가을", age : 23},
   ]
+
+  $("#forLoop").on("click",function(){
+    let result = ''
+    for(let i = 0 ; i < data.length; i++){
+      result = result + `<tr>
+            <td>${i+1}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].age}</td>
+           </tr>`
+    }
+
+    $("#forOut").html(result)
+  })
+
+  $("#forEach").on("click",function(){
+    let result = ''
+    data.forEach((x,i)=>{
+      result = result + `<tr>
+            <td>${i+1}</td>
+            <td>${x.name}</td>
+            <td>${x.age}</td>
+           </tr>`
+    })
+    $("#forEachView").html(result)
+  })
+
+  $("#JQueryEach").on("click",function(){
+    let result = ''
+    $.each(data,function(i,x){
+      result = result + `<tr>
+            <td>${i+1}</td>
+            <td>${x.name}</td>
+            <td>${x.age}</td>
+           </tr>`
+    })
+    $("#JqueryEachView").html(result)
+  })
 })
